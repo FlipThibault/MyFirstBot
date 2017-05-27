f157aadf9a068595498f74558a1ed6d8f046f1ef
Philippe Thibault <philippe.thibault@pj.ca>
2017-05-27 11:44:21 -0400

    - handle messages using the botpress ‘hear’ api rather than using rivescript - remove rivescript

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const _ = require('lodash')
 module.exports = function(bp) {
 
   bp.middlewares.load()
+
+  bp.hear({ platform: 'facebook',type: 'message', text: 'stop' }, (event, next) => {
+    bp.messenger.sendText(event.user.id, 'Thanks for playing, come back anytime. !! I win though :) Just start typing to play again')
+  })
+
+  //currently listening just to facebook messenger and repeat
+  bp.hear({ platform: 'facebook',type: 'message', text: /.+/i }, (event, next) => {
+    bp.messenger.sendText(event.user.id, event.text)
+  })
   
 }
